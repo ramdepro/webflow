a7af8ef50dabc384c7ba2b41e461bf8c8ab984c5
ramdepro <ramdepro@gmail.com>
2016-12-28 14:16:24 +0530

    Add files via upload

---
+++ web flow/src/main/java/com/niit/webfow/example/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.niit.webfow.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String indexPage()
+	{
+		return "index";
+	}
+
+}
